46d74c96c21126cf89c576428c921407c6e7fafd
Mary <mary@Sofa2.local>
2019-01-03 23:23:16 -0700

    Rewrote the structure

--- polls/urls.py
+++ polls/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('HH', views.index, name='index'),
     path('map', views.kala, name='kala'),
     path('questions', views.question_list, name='question_list'),
-    path('menstruation', TemplateView.as_view(template_name="menstruation.html"), name='menstruation'),
+    path('menstruation', TemplateView.as_view(template_name="menstruation/main.html"), name='menstruation'),
+    path('menstruation/basics', TemplateView.as_view(template_name="menstruation/subpages/basics.html"), name='menstruation_basics'),
     path('obstacles', TemplateView.as_view(template_name="obstacles.html"), name='obstacles'),
+    path('stigma', TemplateView.as_view(template_name="stigma.html"), name='stigma'),
+    path('about', TemplateView.as_view(template_name="about.html"), name='about'),
+     path('menstruation/extratips', TemplateView.as_view(template_name="menstruation/subpages/extratips.html"), name='menstruation_extratips'),
+      path('menstruation/products', TemplateView.as_view(template_name="menstruation/subpages/products.html"), name='menstruation_products'),
+       path('menstruation/professionals', TemplateView.as_view(template_name="menstruation/subpages/professionals.html"), name='menstruation_professionals'),
 ]
